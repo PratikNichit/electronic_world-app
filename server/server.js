153e6adbd16f26516cad4ef534dbcccd75e50c85
Pratik Nichit <nichitpratik2004@gmail.com>
2025-02-20 12:38:14 +0530

    connection url issue solved

--- server/server.js
+++ server/server.js
@@ -7,4 +7,6 @@ require("dotenv").config();
 mongoose
   .connect(process.env.DEV_CONNECTION_URL)
   .then(() => console.log("Connected to the MongoDB Atals..."))
-  .catch((err) => console.error("could not connect to the mongoDB...", err));
+  .catch((err) =>
+    console.error("could not connect to the mongoDB Atals...", err)
+  );
