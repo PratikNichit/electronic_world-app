fe1407f0461d7f8ac08e83b161d528f922ece339
Pratik Nichit <nichitpratik2004@gmail.com>
2025-02-20 12:21:56 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DEV_CONNECTION_URL)
+  .then(() => console.log("Connected to the MongoDB Atals..."))
+  .catch((err) => console.error("could not connect to the mongoDB...", err));
