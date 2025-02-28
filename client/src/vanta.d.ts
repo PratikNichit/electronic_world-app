acb8c69ce704201401add1b1a58651a496ea02f2
Pratik <pratikdharam0053@gmail.com>
2025-02-28 23:04:00 +0530

    added vanta library for 'net' view

---
+++ client/src/vanta.d.ts
@@ -0,0 +1,5 @@
+declare module 'vanta/dist/vanta.birds.min';
+declare module 'vanta/dist/vanta.net.min';
+declare module 'vanta/dist/vanta.waves.min';
+declare module 'vanta/dist/vanta.cells.min';
+declare module 'vanta/dist/vanta.globe.min';
